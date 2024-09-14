0f3e420b6ce54f103977cc61c0449916a82287cd
Isaac Yu <yuisaac@outlook.com>
2024-09-14 13:02:15 -0400

    geese page with anger levels

--- goose-peek/src/App.js
+++ goose-peek/src/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './sections/landing/Landing.jsx';
 import Stats from './sections/stats/Stats.jsx';
 import Task from './sections/task/Task.jsx';
+import Geese from './sections/geese/Geese.jsx';
 import data from './sections/navbar/data.js';
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path={data[0].link} element={<Landing />} />
         <Route path={data[1].link} element={<Task />} />
-        <Route path={data[2].link} element={<Stats />} />
+        <Route path={data[2].link} element={<Geese />} />
+        <Route path={data[3].link} element={<Stats />} />
       </Routes>
     </Router>
   );
--- goose-peek/src/sections/navbar/data.js
+++ goose-peek/src/sections/navbar/data.js
@@ -1,7 +1,8 @@
 const data = [
     { id: 0, link: "/", title: "landing"},
     { id: 1, link: "/tasks", title: "tasks" },
-    { id: 2, link: "/stats", title: "stats" },
+    { id: 2, link: "/geese", title: "geese" },
+    { id: 3, link: "/stats", title: "stats" },
   ];
   
   export default data;
