a6e5f1824131862e4adeeaf5af2654e557c82efe
Isaac Yu <yuisaac@outlook.com>
2024-09-14 18:13:33 -0400

    update data.js

--- goose-peek/src/sections/geese/data.js
+++ goose-peek/src/sections/geese/data.js
@@ -4,11 +4,6 @@ const data = [
     { id: 2, path: "/geese-moods/3.jpg"},
     { id: 3, path: "/geese-moods/4.jpg"},
     { id: 4, path: "/geese-moods/5.jpg"},
-    { id: 5, path: "/geese-moods/6.jpg"},
-    { id: 6, path: "/geese-moods/7.jpg"},
-    { id: 7, path: "/geese-moods/8.png"},
-    { id: 8, path: "/geese-moods/9.jpg"},
-    { id: 9, path: "/geese-moods/10.jpg"},
 ];
   
   export default data;
