11d5503c9cfabb01054fd84d9c0f5479834ab95e
maleehaMirza <maleeha.mirza2004@gmail.com>
2024-09-14 18:02:49 -0400

    tasks page changes

--- goose-peek/src/sections/landing/Landing.jsx
+++ goose-peek/src/sections/landing/Landing.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {View, Switch, StyleSheet} from 'react-native';
-import  Navbar from '../navbar/Navbar.jsx';
+import React, { useState, useEffect } from 'react';
+import { View, Switch, StyleSheet } from 'react-native';
+import Navbar from '../navbar/Navbar.jsx';
 import './landing.css';
 // import background from '../../../public/background.js';
 
@@ -15,19 +15,19 @@ const Landing = () => {
 
   return (
     <section className='landing_container'>
-        <h1>GOOSE PEEK</h1>
-        <img id='logo' src={process.env.PUBLIC_URL + '/geese.jpg'} alt="logo" />
-        <p>Are you ready to lock in my little goosling?</p>
-        <View style={styles.container}>
-          <Switch
-            trackColor={{false: '#767577', true: '#81b0ff'}}
-            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-          />
-        </View>
-        <Navbar/>
+      <h1>GOOSE PEEK</h1>
+      <img id='logo' src={process.env.PUBLIC_URL + '/geese.jpg'} alt="logo" />
+      <p>Are you ready to lock in my little gooling?</p>
+      <View style={styles.container}>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      <Navbar />
     </section>
 
   );
