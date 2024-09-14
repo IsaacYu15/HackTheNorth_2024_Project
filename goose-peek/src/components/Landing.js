59a39efb5741e6441c02295ac2a113551aa11ea3
Isaac Yu <yuisaac@outlook.com>
2024-09-14 10:28:47 -0400

    landing page

---
+++ goose-peek/src/components/Landing.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {View, Switch, StyleSheet} from 'react-native';
+import './Landing.css';
+
+const Landing = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+    <section id='landing_container'>
+        <h1>GOOSE PEEK</h1>
+        <img id='logo' src={process.env.PUBLIC_URL + '/geese.jpg'} alt="logo" />
+        <p>Are you ready to lock in my little goosling?</p>
+        <View style={styles.container}>
+          <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
+    </section>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Landing;
