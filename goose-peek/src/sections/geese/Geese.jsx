9c1bf5ab9d69dc00d60de17575c953127cb87d33
Isaac Yu <yuisaac@outlook.com>
2024-09-14 14:18:21 -0400

    started with geese stuff

--- goose-peek/src/sections/geese/Geese.jsx
+++ goose-peek/src/sections/geese/Geese.jsx
@@ -1,16 +1,30 @@
 import './geese.css';
 import Navbar from '../navbar/Navbar.jsx';
+import {Button} from 'react-native';
+import data from './data.js';
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Stats = () => {
-  const [angerLevel, setAngerLevel] = useState(0);
+  const [angerLevel, setAngerLevel] = useState(5);
+
+  function decreaseAnger ()
+  {
+    if (angerLevel > 0)
+    {
+        setAngerLevel(angerLevel - 1);
+    }
+  }
 
   return (
     <section className='geese_container'>
         <h1>Geese</h1>
         <p>Current Anger: {angerLevel}</p>
+        <img id='geese' src={process.env.PUBLIC_URL + data[angerLevel].path} alt="logo" />
+
+        <Button onPress={decreaseAnger} title="calm goose"/>
         <Navbar/>
+        
     </section>
 
   );
---
+++ goose-peek/src/sections/geese/data.js
@@ -0,0 +1,14 @@
+const data = [
+    { id: 0, path: "/geese-moods/1.jpg"},
+    { id: 1, path: "/geese-moods/2.jpg"},
+    { id: 2, path: "/geese-moods/3.jpg"},
+    { id: 3, path: "/geese-moods/4.jpg"},
+    { id: 4, path: "/geese-moods/5.jpg"},
+    { id: 5, path: "/geese-moods/6.jpg"},
+    { id: 6, path: "/geese-moods/7.jpg"},
+    { id: 7, path: "/geese-moods/8.png"},
+    { id: 8, path: "/geese-moods/9.jpg"},
+    { id: 9, path: "/geese-moods/10.jpg"},
+];
+  
+  export default data;
